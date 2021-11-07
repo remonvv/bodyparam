4fb6f03b843205065e670bbe79e08bd2f17336c7
Remon van Vliet <remonvanvliet@gmail.com>
2021-11-07 23:01:17 +0100

    Dabbling with documentation

--- src/main/java/org/remonvv/bodyparam/BodyParam.java
+++ src/main/java/org/remonvv/bodyparam/BodyParam.java
@@ -1,3 +1,24 @@
+/**
+ * Copyright (c) 2021 the original author or authors.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package org.remonvv.bodyparam;
 
 import static org.remonvv.bodyparam.NameMatchingMode.IGNORE_CASE_AND_NON_ALPHA_NUMERIC;
@@ -10,6 +31,28 @@ import java.lang.annotation.Target;
 
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.web.bind.annotation.ValueConstants;
+
+/**
+ * <p>
+ * Annotation used to mark web controller method parameters as parameters that
+ * require request body values to be injected into the parameter. This
+ * annotation works conceptually similar to {@code @PathVariable} and
+ * {@code @QueryParam}.
+ *
+ * <p>
+ * {@code @BodyParam} supports reading from request bodies that use either JSON
+ * or XML data formats.
+ *
+ * <p>
+ * <h1>JSON</h1> JSON request bodies are expected to be valid JSON.
+ *
+ * <p>
+ * <h1>XML</h1> XML request bodies are expected to be valid XML. Injecting XML
+ * tag attributes is not supported.
+ *
+ *
+ * @author Remon van Vliet
+ */
 
 @Target(ElementType.PARAMETER)
 @Retention(RetentionPolicy.RUNTIME)
@@ -57,7 +100,7 @@ public @interface BodyParam {
 	boolean required() default true;
 
 	/**
-	 * Alias for the path attribute
+	 * Alias for the path attribute.
 	 */
 	@AliasFor("path")
 	String value() default ValueConstants.DEFAULT_NONE;
